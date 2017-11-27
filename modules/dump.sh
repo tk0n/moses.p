0e6eb511d036757877ada18c7989cf3737932b0a
Brian Faust <hello@brianfaust.me>
2017-11-27 12:16:45 +0200

    keep N amount of snapshots
    Signed-off-by: Brian Faust <hello@brianfaust.me>

--- modules/dump.sh
+++ modules/dump.sh
@@ -3,8 +3,18 @@
 snapshooter_dump()
 {
     cd $snapshot_dir
+
+    # create a new snapshot
     pg_dump -Fc ark_${network} > latest
 
-    rm -f current
-    mv latest current
+    # move old snapshot
+    current_file=current # $(ls -t | head -n1)
+    current_date=$(stat -c %Y $current_file)
+    mv $current_file $current_date
+
+    # move new snapshot
+    mv latest $current_file
+
+    # delete all but the 10 recent snapshots
+    ls -r | tail -n +10 | xargs rm
 }
