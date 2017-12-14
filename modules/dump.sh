b2e4ecbc4a1074e767894789e0b66e82f99d9369
Brian Faust <hello@brianfaust.me>
2017-12-14 09:30:55 +0200

    use height and timestamp as filename

--- modules/dump.sh
+++ modules/dump.sh
@@ -12,31 +12,34 @@
 moses_dump()
 {
     # make sure postgres is running
-    process_postgres=$(pgrep -a postgres | awk '{print $1}')
+    local process_postgres=$(pgrep -a postgres | awk '{print $1}')
 
     if [ -z "$process_postgres" ]; then
         sudo service postgresql start
     fi
 
+    # grab the height and timestamp before we dump
+    local port=4001
+    if [[ $network == 'devnet' ]]; then
+        port=4002
+    fi
+
+    local height=$(curl "http://localhost:${port}/api/loader/status/sync" -s | jq .height)
+    local timestamp=$(date +%s)
+    local target_file="${height}_${timestamp}"
+
     # go into the snapshot dir
     cd $snapshot_dir
 
     # create a new snapshot
-    pg_dump -Fc ark_${network} > latest
+    pg_dump -Fc ark_${network} > $target_file
 
-    # dump failed, exit so we don't overwrite the current one
+    # dump failed
     if [ $? -ne 0 ]; then
+        rm -f $target_file
         error "Dump failed..."
         return 1
     fi
-
-    # move old snapshot
-    current_file=current # $(ls -t | head -n1)
-    current_date=$(stat -c %Y $current_file)
-    mv $current_file $current_date
-
-    # move new snapshot
-    mv latest $current_file
 
     # delete all but the 10 recent snapshots
     ls -t | tail -n +$snapshot_amount | xargs rm --
