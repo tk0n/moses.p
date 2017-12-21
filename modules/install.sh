6fd923ddf482506e8912d6d01f843a9c7475d88c
Brian Faust <faustbrian@users.noreply.github.com>
2017-12-21 08:26:03 +0200

    add jq and pm2 to install

--- modules/install.sh
+++ modules/install.sh
@@ -42,5 +42,21 @@ moses_install()
         info "ufw doesn't exists..."
     fi
 
+    if [[ -z $(command -v jq) ]]; then
+        heading "Installing jq..."
+        sudo apt-get -qq install jq >> $moses_log 2>&1
+        success "Installation OK."
+    else
+        info "jq already exists..."
+    fi
+
+    if [[ -z $(command -v pm2) ]]; then
+        heading "Installing pm2..."
+        npm install pm2 -g >> $moses_log 2>&1
+        success "Installation OK."
+    else
+        info "pm2 already exists..."
+    fi
+    
     success "Installation complete!"
 }
